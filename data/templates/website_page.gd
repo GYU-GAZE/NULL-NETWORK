e12b91543296c74ea4a6d191b7d9edcf3cb450a3
Gabriel Moreno Dal Ponte do Rosário <gyugazeworks@gmail.com>
2026-07-09 23:09:25 -0300

    Make browser pages scene-only

--- data/templates/website_page.gd
+++ data/templates/website_page.gd
@@ -1,9 +1,22 @@
 extends Resource
 class_name WebsitePage
 
+@export_category("Route")
 @export var url: String = "null.net"
+@export var match_as_prefix: bool = false
+
+@export_category("Browser Metadata")
 @export var page_title: String = "New Page"
 @export var favicon: Texture2D
 @export var record_in_browser_history: bool = true
-@export var match_as_prefix: bool = false
-@export var custom_site_scene: PackedScene
+
+@export_category("Page Scene")
+@export var site_scene: PackedScene
+
+
+func has_valid_route() -> bool:
+	return not url.strip_edges().is_empty()
+
+
+func has_site_scene() -> bool:
+	return site_scene != null
