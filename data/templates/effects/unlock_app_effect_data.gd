0af00fc4dd5a90efe6fd44b69ef23a1d0a808426
Gabriel Moreno Dal Ponte do Rosário <gyugazeworks@gmail.com>
2026-08-02 21:42:38 -0300

    Phase 4: add data/templates/effects/unlock_app_effect_data.gd

---
+++ data/templates/effects/unlock_app_effect_data.gd
@@ -0,0 +1,39 @@
+extends GameEffectData
+class_name UnlockAppEffectData
+
+
+@export var app_id: String = ""
+
+
+func _apply_effect(_context: GameEffectContext) -> bool:
+	var clean_id: String = app_id.strip_edges()
+
+	if ContentRegistry.get_app(clean_id) == null:
+		return false
+
+	CampaignState.install_app(clean_id)
+	return true
+
+
+func _validate_effect() -> PackedStringArray:
+	return _validate_registered_id(
+		ContentRegistry.CATEGORY_APPS,
+		app_id,
+		"app_id"
+	)
+
+
+func _validate_registered_id(
+	category: StringName,
+	content_id: String,
+	field_name: String
+) -> PackedStringArray:
+	var errors := PackedStringArray()
+	var clean_id: String = content_id.strip_edges()
+
+	if clean_id.is_empty():
+		errors.append("%s cannot be empty." % field_name)
+	elif not ContentRegistry.has(category, clean_id):
+		errors.append("%s '%s' is not registered." % [field_name, clean_id])
+
+	return errors
