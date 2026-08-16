bc470102d05da2fef6a4e9d5967b4ed8f1a5c25c
Gabriel Moreno Dal Ponte do Rosário <gyugazeworks@gmail.com>
2026-08-16 01:14:36 -0300

    Add operator appearance catalog resource

---
+++ data/templates/operator/appearance_catalog_data.gd
@@ -0,0 +1,35 @@
+extends Resource
+class_name AppearanceCatalogData
+
+@export var options: Array[AppearanceOptionData] = []
+
+func get_options(category: AppearanceOptionData.Category) -> Array[AppearanceOptionData]:
+	var result: Array[AppearanceOptionData] = []
+	for option: AppearanceOptionData in options:
+		if option != null and option.category == category:
+			result.append(option)
+	return result
+
+func get_option(option_id: String) -> AppearanceOptionData:
+	var clean_id := option_id.strip_edges()
+	for option: AppearanceOptionData in options:
+		if option != null and option.option_id == clean_id:
+			return option
+	return null
+
+func validate_data() -> PackedStringArray:
+	var errors := PackedStringArray()
+	var ids := PackedStringArray()
+	for option: AppearanceOptionData in options:
+		if option == null:
+			errors.append("Appearance catalog contains a null option.")
+			continue
+		errors.append_array(option.validate_data())
+		if ids.has(option.option_id):
+			errors.append("Appearance catalog repeats option '%s'." % option.option_id)
+		else:
+			ids.append(option.option_id)
+	for category in AppearanceOptionData.Category.values():
+		if get_options(category).is_empty() and category not in [AppearanceOptionData.Category.HAT, AppearanceOptionData.Category.FACIAL_ACCESSORY]:
+			errors.append("Appearance catalog has no required option for category %s." % AppearanceOptionData.Category.keys()[category])
+	return errors
